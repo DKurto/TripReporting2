695e630a0bdc60a0cc28f8338d363c2574393455
yauheni.ulyanovich <yauheni.ulyanovich@leverx.com>
2016-07-19 16:32:06 +0300

    Add NewReport component

--- src/containers/NewReport.js
+++ src/containers/NewReport.js
@@ -6,6 +6,14 @@ export default class Reports extends Component {
         <div className="panel panel-primary">
           <div className="panel-heading">
             <h3 className="panel-title">New Report</h3>
+            <input type="text" className="date-input" placeholder="Enter Date"></input>
+            <input type="text" className="location-input" placeholder="Enter Location"></input>
+            <button className="btn btn-primary" onClick={this.addNewCharge}>Add New Charge</button>
+          </div>
+          <SecondScreenCharge />
+          <div className="panel-footer">
+            <button className="btn btn-primary" onClick={this.saveButton}>Save Button</button>
+            <button className="btn btn-primary" onClick={this.cancelButton}>Cancel Button</button>
           </div>
         </div>
     );
